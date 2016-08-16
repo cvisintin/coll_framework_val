08a191ed3b95f4935d8b976b00535b5dfc5cea0a
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-08-16 14:43:13 -0700

    modified write paths

--- R/carcass_data_processing.R
+++ R/carcass_data_processing.R
@@ -1,10 +1,10 @@
-library("rgdal")
-library("rgeos")
-library("maptools")
-library("sp")
-library("data.table")
+require("rgdal")
+require("rgeos")
+require("maptools")
+require("sp")
+require("data.table")
 
-carcass.raw.data <- as.data.table(read.delim("Asset_Vision_Pavement_Cleaning.csv", header=T, sep=","))
+carcass.raw.data <- as.data.table(read.delim("data/Asset_Vision_Pavement_Cleaning.csv", header=T, sep=","))
 
 carcass.data <- carcass.raw.data[Comments %like% "roo" & Longitude != "" & Latitude != "", .(Created_Date,Longitude,Latitude)]
 
@@ -13,4 +13,4 @@ proj4string(carcass.data) <- CRS("+init=epsg:4326") # WGS84
 CRS.mga9455 <- CRS("+init=epsg:28355") # MGA9455
 carcass.data.mga9455 <- spTransform(carcass.data, CRS.mga9455)
 
-writeOGR(carcass.data.mga9455, ".", "VIC_GDA9455_FAUNA_EGK_WESTERNHWY", driver="ESRI Shapefile")
+writeOGR(carcass.data.mga9455, "data/", "VIC_GDA9455_FAUNA_EGK_WESTERNHWY", driver="ESRI Shapefile")
