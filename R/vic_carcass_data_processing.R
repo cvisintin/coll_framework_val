7b94bfe96057c953600d7123a439412850f0c80c
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-09-26 17:48:17 +1000

    added syntax to write data to database

--- R/vic_carcass_data_processing.R
+++ R/vic_carcass_data_processing.R
@@ -3,6 +3,10 @@ require(rgeos)
 require(maptools)
 require(sp)
 require(data.table)
+require(RPostgreSQL)
+
+drv <- dbDriver("PostgreSQL")  #Specify a driver for postgreSQL type database
+con <- dbConnect(drv, dbname="qaeco_spatial", user="qaeco", password="Qpostgres15", host="boab.qaeco.com", port="5432")  #Connection to database server on Boab
 
 carcass.raw.data <- as.data.table(read.delim("data/Asset_Vision_Pavement_Cleaning.csv", header=T, sep=","))
 
@@ -14,3 +18,12 @@ CRS.mga9455 <- CRS("+init=epsg:28355") # MGA9455
 carcass.data.mga9455 <- spTransform(carcass.data, CRS.mga9455)
 
 writeOGR(carcass.data.mga9455, "data/", "VIC_GDA9455_FAUNA_EGK_WESTERNHWY", driver="ESRI Shapefile")
+
+writeOGR(carcass.data.mga9455,
+         driver = "PostgreSQL",
+         "PG:dbname=qaeco_spatial user=qaeco password=Qpostgres15 host=boab.qaeco.com port=5432",
+         layer = "gis_victoria.vic_gda9455_fauna_egk_westerndist",
+         layer_options = c("GEOMETRY_NAME=geom","FID=id"),
+         overwrite_layer=TRUE)
+
+dbGetQuery(con,"select UpdateGeometrySRID('gis_victoria', 'vic_gda9455_fauna_egk_westerndist', 'geom', 28355);")
