1174d2213fb259a014ec0bbb2e40a8955e39bed7
Casey Visintin <cvisintin@student.unimelb.edu.au>
2017-02-19 11:09:13 +1100

    commenting

--- R/validation_plots.R
+++ R/validation_plots.R
@@ -307,43 +307,43 @@ ggplot() +
 dev.off()
 
 #######################################################################################
-
-y <- coll.glm$data$y
-p <- predict(coll.glm, coll.glm$data, type="response")
-
-p.b <- .bincode(p,seq(min(p),max(p),(max(p)-min(p))/10),include.lowest = TRUE)
-
-yp_bins <- data.frame(y,p,p.b)
-
-
-plot.info <- ddply(yp_bins, ~p.b, summarise,
-                   count=length(y),
-                   prop_coll=sum(y)/length(y),
-                   prop_nocoll=(length(y)-sum(y))/length(y),
-                   prop_lo=binom.test(sum(y), length(y), 0.5)$conf.int[1],
-                   prop_hi=binom.test(sum(y), length(y), 0.5)$conf.int[2],
-                   median_p=median(p)
-)
-
-calib <- glm(y~log(p), family=binomial(link=cloglog), data=yp_bins)
-roc <- roc(y,p)
-perform.glm <- rbind(calib_int=calib$coefficients[1], calib_slope=calib$coefficients[2], roc)
-colnames(perform.glm) <- "0.0"
-
-png('figs/calib.png', pointsize = 6, res=300, width = 900, height = 900, bg='transparent')
-ggplot() +
-  #geom_smooth(data=plot.glm, aes(y=y,x=x), formula=y~log(x), method=glm, size = 0.2, colour='black', inherit.aes=FALSE) +
-  geom_line(data=yp_bins, aes(y=invcloglog(calib$coefficients[1]+calib$coefficients[2]*log(p)),x=p)) +
-  geom_pointrange(data=plot.info, aes(x=median_p, y=prop_coll, ymin=prop_lo, ymax=prop_hi), size = 0.2, inherit.aes=FALSE) +
-  geom_text(data=plot.info, aes(x=median_p, y=prop_coll, label=count),hjust=-0.1, vjust=-1, size = 2.0, inherit.aes=FALSE) +
-  geom_segment(aes(x = 0, y = 0, xend = .04, yend = .04), linetype=2, size=0.1, inherit.aes=FALSE) +
-  #coord_flip() +
-  ylab("Observed Rate (proportion in one year)") +
-  xlab("Predicted Rate (proportion in one year)") +
-  theme_bw() +
-  theme(plot.margin=unit(c(.5,0,.1,.1),"cm")) +
-  theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
-  theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
-  theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
-  theme(text = element_text(size = 10))
-dev.off()
+# 
+# y <- coll.glm$data$y
+# p <- predict(coll.glm, coll.glm$data, type="response")
+# 
+# p.b <- .bincode(p,seq(min(p),max(p),(max(p)-min(p))/10),include.lowest = TRUE)
+# 
+# yp_bins <- data.frame(y,p,p.b)
+# 
+# 
+# plot.info <- ddply(yp_bins, ~p.b, summarise,
+#                    count=length(y),
+#                    prop_coll=sum(y)/length(y),
+#                    prop_nocoll=(length(y)-sum(y))/length(y),
+#                    prop_lo=binom.test(sum(y), length(y), 0.5)$conf.int[1],
+#                    prop_hi=binom.test(sum(y), length(y), 0.5)$conf.int[2],
+#                    median_p=median(p)
+# )
+# 
+# calib <- glm(y~log(p), family=binomial(link=cloglog), data=yp_bins)
+# roc <- roc(y,p)
+# perform.glm <- rbind(calib_int=calib$coefficients[1], calib_slope=calib$coefficients[2], roc)
+# colnames(perform.glm) <- "0.0"
+# 
+# png('figs/calib.png', pointsize = 6, res=300, width = 900, height = 900, bg='transparent')
+# ggplot() +
+#   #geom_smooth(data=plot.glm, aes(y=y,x=x), formula=y~log(x), method=glm, size = 0.2, colour='black', inherit.aes=FALSE) +
+#   geom_line(data=yp_bins, aes(y=invcloglog(calib$coefficients[1]+calib$coefficients[2]*log(p)),x=p)) +
+#   geom_pointrange(data=plot.info, aes(x=median_p, y=prop_coll, ymin=prop_lo, ymax=prop_hi), size = 0.2, inherit.aes=FALSE) +
+#   geom_text(data=plot.info, aes(x=median_p, y=prop_coll, label=count),hjust=-0.1, vjust=-1, size = 2.0, inherit.aes=FALSE) +
+#   geom_segment(aes(x = 0, y = 0, xend = .04, yend = .04), linetype=2, size=0.1, inherit.aes=FALSE) +
+#   #coord_flip() +
+#   ylab("Observed Rate (proportion in one year)") +
+#   xlab("Predicted Rate (proportion in one year)") +
+#   theme_bw() +
+#   theme(plot.margin=unit(c(.5,0,.1,.1),"cm")) +
+#   theme(axis.title.x = element_text(margin=unit(c(.3,0,0,0),"cm"))) +
+#   theme(axis.title.y = element_text(margin=unit(c(0,.3,0,0),"cm"))) +
+#   theme(panel.grid.major = element_line(size=0.1),panel.grid.minor = element_line(size=0.1)) +
+#   theme(text = element_text(size = 10))
+# dev.off()
